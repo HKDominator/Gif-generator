ed20ff9778d25c1114d5c95ff6646e9d5a1d6332
Andrei-Darius Bodrogean <bodrogean.andrei@gmail.com>
2025-07-27 13:52:31 +0300

    Changed the loop mode for the imwrite

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ name = "Test3"
 
 for filename in filenames:
     images.append(imageio.imread(filename)) #the imageio.imread method loads the images from their filepath
-imageio.imwrite(f"{name}.gif", images, duration = 300, loop = 1 ) #the loop determines for how many times it loops
+imageio.imwrite(f"{name}.gif", images, duration = 300, loop = 0 ) #the loop determines for how many times it loops
 #loop = 0 infinity otherwise the specified amount
 
 
